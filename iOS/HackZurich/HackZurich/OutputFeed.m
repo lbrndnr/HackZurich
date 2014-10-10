e8e196885777621f898883dd1ad1a9ec6ad75e2f
Patrick Amrein <patrickamrein@Patricks-MacBook-Air.local>
2014-10-11 00:36:22 +0200

    Created OutputFeed Class

---
+++ iOS/HackZurich/HackZurich/OutputFeed.m
@@ -0,0 +1,13 @@
+//
+//  OutputFeed.m
+//  HackZurich
+//
+//  Created by Patrick Amrein on 10/10/14.
+//  Copyright (c) 2014 Laurin Brandner. All rights reserved.
+//
+
+#import "OutputFeed.h"
+
+@implementation OutputFeed
+
+@end
