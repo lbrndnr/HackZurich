b9a49f4c868abeb1af63e5735c1a32dd98ecdd91
Laurin Brandner <mail@laurinbrandner.ch>
2014-10-11 18:25:24 +0200

    Add calendar view controller

--- iOS/HackZurich/HackZurich/AppDelegate.m
+++ iOS/HackZurich/HackZurich/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "FirstViewController.h"
+#import "CalendarViewController.h"
 #import "FeedsViewController.h"
 #import "WebService.h"
 
@@ -36,7 +36,7 @@ NSString* const AppDelegateCurrentUserKey = @"AppDelegateCurrentUser";
     
     UITabBarController* controller = [UITabBarController new];
     
-    UINavigationController* firstViewController = [[UINavigationController alloc] initWithRootViewController:[FirstViewController new]];
+    UINavigationController* firstViewController = [[UINavigationController alloc] initWithRootViewController:[CalendarViewController new]];
     firstViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"First" image:nil selectedImage:nil];
     UINavigationController* secondViewController = [[UINavigationController alloc] initWithRootViewController:[FeedsViewController new]];
     secondViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Second" image:nil selectedImage:nil];
---
+++ iOS/HackZurich/HackZurich/FeedsVisiblityViewController.m
@@ -0,0 +1,74 @@
+//
+//  FeedsVisiblityViewController.m
+//  HackZurich
+//
+//  Created by Laurin Brandner on 11/10/14.
+//  Copyright (c) 2014 Laurin Brandner. All rights reserved.
+//
+
+#import "FeedsVisiblityViewController.h"
+#import "WebService.h"
+
+@interface FeedsVisiblityViewController ()
+
+-(void)reloadSelection;
+
+@end
+@implementation FeedsVisiblityViewController
+
+-(void)setSelectedFeeds:(NSArray *)selectedFeeds {
+    if (![_selectedFeeds isEqualToArray:selectedFeeds]) {
+        _selectedFeeds = selectedFeeds;
+        
+        [self reloadSelection];
+    }
+}
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = NSLocalizedString(@"Visible Feeds", nil);
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [WebService sharedService].feeds.count;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    Feed* feed = [WebService sharedService].feeds[indexPath.row];
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class]) forIndexPath:indexPath];
+    
+    cell.textLabel.text = feed.name;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    Feed* feed = [WebService sharedService].feeds[indexPath.row];
+    
+    NSMutableArray* newSelectedFeeds = self.selectedFeeds.mutableCopy ?: [NSMutableArray new];
+    if ([newSelectedFeeds containsObject:feed]) {
+        [newSelectedFeeds removeObject:feed];
+    }
+    else {
+        [newSelectedFeeds addObject:feed];
+    }
+    self.selectedFeeds = newSelectedFeeds;
+}
+
+-(void)reloadSelection {
+    for (NSUInteger r = 0; r < [self tableView:self.tableView numberOfRowsInSection:0]; r++) {
+        Feed* feed = [WebService sharedService].feeds[r];
+        UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:r inSection:0]];
+        cell.accessoryType = ([self.selectedFeeds containsObject:feed]) ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    }
+}
+
+@end
