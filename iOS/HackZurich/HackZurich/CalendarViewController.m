b1dda3133ddf71c1d72179de3f307c3c6701f787
Laurin Brandner <mail@laurinbrandner.ch>
2014-10-11 19:56:02 +0200

    Improve calendar view controller

--- iOS/HackZurich/HackZurich/CalendarViewController.m
+++ iOS/HackZurich/HackZurich/CalendarViewController.m
@@ -8,14 +8,18 @@
 
 #import "CalendarViewController.h"
 #import "FeedsVisiblityViewController.h"
+#import "XbICalendar.h"
 
 @interface CalendarViewController ()
 
+@property (nonatomic, strong) NSArray* sectionDates;
 @property (nonatomic, strong) NSArray* events;
 @property (nonatomic, weak) FeedsVisiblityViewController* visiblityViewController;
 
 -(void)presentFeedVisibilityViewController:(id)sender;
 -(void)dismissFeedVisibilityViewController:(id)sender;
+
+-(void)downloadCalenderDataWithURL:(NSURL *)URL withCompletion:(void (^)(NSData*))completion;
 
 @end
 @implementation CalendarViewController
@@ -30,6 +34,45 @@
     [self.tableView registerClass:cellClass forCellReuseIdentifier:NSStringFromClass(cellClass)];
 }
 
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    NSURL* URL = [NSURL URLWithString:@"https://www.google.com/calendar/ical/uuvar5p1a250iuu4ntg1mebm5k%40group.calendar.google.com/public/basic.ics"];
+    [self downloadCalenderDataWithURL:URL withCompletion:^(NSData* data) {
+        NSString* content = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        XbICVCalendar * vCalendar =  [XbICVCalendar vCalendarFromString:content];
+        
+        NSMutableArray* newSectionDates = [NSMutableArray new];
+        NSMutableArray* newEvents = [NSMutableArray new];
+        
+        NSArray* allEvents = [vCalendar componentsOfKind:ICAL_VEVENT_COMPONENT];
+        allEvents = [allEvents sortedArrayUsingSelector:@selector(dateStart)];
+        
+        NSInteger lastDay = 0;
+        NSInteger lastMonth = 0;
+        NSInteger lastYear = 0;
+        
+        for (XbICVEvent* event in allEvents) {
+            NSDateComponents* components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay|NSCalendarUnitMonth|NSCalendarUnitYear fromDate:event.dateStart];
+            NSInteger day = [components day];
+            NSInteger month = [components month];
+            NSInteger year = [components year];
+            
+            if (lastDay != day || lastMonth != month || lastYear != year) {
+                [newSectionDates addObject:event.dateStart];
+                [newEvents addObject:[NSMutableArray arrayWithObject:event]];
+            }
+            else {
+                [((NSMutableArray*)newEvents.lastObject) addObject:event];
+            }
+        }
+        
+        self.sectionDates = newSectionDates;
+        self.events = newEvents;
+        [self.tableView reloadData];
+    }];
+}
+
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return self.events.count;
 }
@@ -39,8 +82,10 @@
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    XbICVEvent* event = ((NSArray*)self.events[indexPath.section])[indexPath.row];
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class]) forIndexPath:indexPath];
     
+    cell.textLabel.text = event.description;
     
     return cell;
 }
@@ -56,6 +101,17 @@
 
 -(void)dismissFeedVisibilityViewController:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)downloadCalenderDataWithURL:(NSURL *)URL withCompletion:(void (^)(NSData*))completion {
+    if (completion) {
+        NSURLSessionDataTask* task = [[NSURLSession sharedSession] dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completion(data);
+            });
+        }];
+        [task resume];
+    }
 }
 
 @end
